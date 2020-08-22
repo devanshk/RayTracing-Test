afd323c552c3854184e3da1c3f5f517cd08e6148
devanshk <dkukreja@fb.com>
2020-08-22 12:12:04 -0700

    added more controls

--- dist/ray_tracer.dev.js
+++ dist/ray_tracer.dev.js
@@ -33,24 +33,7 @@ function preload() {}
 function create() {
   graphics = this.add.graphics();
   this.cameras.main.setBackgroundColor(0x0);
-
-  for (var i = 0; i < num_rects; i++) {
-    var width = Math.floor(Math.pow(Math.random(), 3) * 251 + 100);
-    var height = Math.floor(Math.pow(Math.random(), 3) * 251 + 100);
-    var x = Math.floor(Math.random() * (screenWidth - width));
-    var y = Math.floor(Math.random() * (screenHeight - height));
-    var rect = new Phaser.Geom.Rectangle(x, y, width, height);
-    rects.push(rect);
-  }
-
-  for (var i = 0; i < num_circles; i++) {
-    var radius = Math.floor(Math.pow(Math.random(), 3) * 251 + 100);
-    var x = Math.floor(Math.random() * (screenWidth - radius));
-    var y = Math.floor(Math.random() * (screenHeight - radius));
-    var circle = new Phaser.Geom.Circle(x, y, radius);
-    circles.push(circle);
-  }
-
+  gen_environment();
   center = new Phaser.Geom.Point(this.cameras.main.centerX, this.cameras.main.centerY);
   debug_txt = this.add.text(10, 10, 'hello there', {
     color: '#ddd'
@@ -64,6 +47,47 @@ function create() {
   this.input.keyboard.on('keyup-' + 'S', function (event) {
     num_rays = num_rays < 1 ? 0.5 : num_rays / 2;
   });
+  this.input.keyboard.on('keyup-' + 'Q', function (event) {
+    num_rects *= 2;
+    gen_environment();
+  });
+  this.input.keyboard.on('keyup-' + 'A', function (event) {
+    num_rects = num_rects < 1 ? 0.5 : num_rects / 2;
+    gen_environment();
+  });
+  this.input.keyboard.on('keyup-' + 'E', function (event) {
+    num_circles *= 2;
+    gen_environment();
+  });
+  this.input.keyboard.on('keyup-' + 'D', function (event) {
+    num_circles = num_circles < 1 ? 0.5 : num_circles / 2;
+    gen_environment();
+  });
+  this.input.keyboard.on('keyup-' + 'SPACE', function (event) {
+    gen_environment();
+  });
+}
+
+function gen_environment() {
+  rects = [];
+  circles = [];
+
+  for (var i = 0; i < Math.floor(num_rects); i++) {
+    var width = Math.floor(Math.pow(Math.random(), 3) * 251 + 100);
+    var height = Math.floor(Math.pow(Math.random(), 3) * 251 + 100);
+    var x = Math.floor(Math.random() * (screenWidth - width));
+    var y = Math.floor(Math.random() * (screenHeight - height));
+    var rect = new Phaser.Geom.Rectangle(x, y, width, height);
+    rects.push(rect);
+  }
+
+  for (var i = 0; i < Math.floor(num_circles); i++) {
+    var radius = Math.floor(Math.pow(Math.random(), 3) * 251 + 100);
+    var x = Math.floor(Math.random() * (screenWidth - radius));
+    var y = Math.floor(Math.random() * (screenHeight - radius));
+    var circle = new Phaser.Geom.Circle(x, y, radius);
+    circles.push(circle);
+  }
 }
 
 function update() {
@@ -85,7 +109,7 @@ function update() {
     center.setTo(center.x, center.y + speed);
   }
 
-  debug_txt.setText('Rays: ' + Math.floor(num_rays) + ' (use W + S to change)');
+  debug_txt.setText('Rays: ' + Math.floor(num_rays) + ' (use W + S to change)' + '\nRandomize Shapes: Spacebar' + '\nRects: ' + Math.floor(num_rects) + ' (use Q + A to change' + '\nCircles: ' + Math.floor(num_circles) + ' (use E + D to change');
   render();
 }
 
